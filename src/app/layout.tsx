cf807838a3d2e833390b0987a82e6b2c21eba2bc
nikkowalow <nikko@kowalow.com>
2025-05-18 03:18:12 -0500

    updated browser table title and icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,19 @@ import "./globals.css";
 
 
 export const metadata = {
-  title: "Halo | Event Ticketing Platform",
-  description: "Seamless event creation and ticketing in one app",
+  title: "Nikko Kowalow",
+  description: "Dev portfolio.",
+  icons: {
+    icon: "/logo.ico",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
       <html lang="en">
+        <head>
+            <link rel="logo" href="/logo.png" />
+        </head>
         <body>
           {children}
         </body>
